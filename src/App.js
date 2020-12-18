803718e7025cd58b34471e1a63cabc4840062546
Kacper <lorenstenlelak@gmail.com>
2020-12-18 19:33:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -40,9 +40,6 @@ function App() {
           <br />
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
           nisi ut aliquip ex ea commodo consequat.
-          <br />
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur.
         </div>
 
         <div className="money">
