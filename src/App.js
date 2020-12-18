0d06cff4db3b157e27340ab948a59d0929de00a2
Kacper <lorenstenlelak@gmail.com>
2020-12-18 11:56:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,80 @@
-import React, { useRef, useEffect } from 'react';
-import './App.css';
-import { ReactComponent as Money } from './money.svg';
-import gsap from 'gsap';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import { ReactComponent as Money } from "./money.svg";
+import gsap from "gsap";
 
 function App() {
-
   const wrapper = useRef(null);
 
   useEffect(() => {
+    const floor = document.querySelectorAll(".floor");
 
-    const floor = document.querySelectorAll('.floor');
+    const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
 
-    const tl = gsap.timeline( { defaults: { ease: 'power3.inOut' } } );
+    gsap.set([], { opacity: 0 });
 
-    gsap.set([], { opacity: 0 } );
-
-    tl.fromTo(floor, { opacity: 0 }, { opacity: 1, repeat: -1 })
-
+    tl.fromTo(floor, { opacity: 0 }, { opacity: 1, repeat: -1 });
   });
 
   return (
     <div ref={ wrapper }>
-    <button className='newsletter'><b>Join to newsletter</b></button>
+      <button className="newsletter">
+        <b>Join to newsletter</b>
+      </button>
       <header>
-        <div className='nav'>
-          <button className='login'><b>Login with Steam </b>it's Free</button>
+        <div className="nav">
+          <button className="title">
+            Jackpot<span className="osw"> osw</span>
+          </button>
+          <button className="login">
+            <b>Login with Steam </b>it's Free
+          </button>
         </div>
-        <div className='money'>
+
+        <div className="headerText">
+          <h1>Co to Jackpot?!</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <br />
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+          <br />
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur.
+        </div>
+
+        <div className="money">
           <div>
-            <h1 className='moneyTitle'>Jackpot
-            <span className='floor'>_</span></h1>
-            <button className='moneyButton'><b>Join now</b></button>
+            <h1 className="moneyTitle">
+              Play
+              <span className="floor">_</span>
+              now
+            </h1>
+            <button className="moneyButton">
+              <b>Join now</b>
+            </button>
           </div>
-          <Money className='moneyPhoto' />
+          <Money className="moneyPhoto" />
         </div>
       </header>
       <article>
-        <h1 className='conatinerTwoTitle'>Lorem ipsum</h1>
-        <p className='containerTwoText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <h1 className="conatinerTwoTitle">Lorem ipsum</h1>
+        <p className="containerTwoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+          <br /> Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur.
+        </p>
       </article>
       <footer>
         <ul>
-          <li>Created by Jackpot company inc.</li>
+          <li>Created by Jackpot OSW. company inc.</li>
         </ul>
       </footer>
     </div>
   );
 }
-
 
 export default App;
